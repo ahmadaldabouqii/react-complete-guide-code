6b3762d4e294baea4cb0c07f2c0032e65b1f503a
ahmadaldabouqii <aldabouqiahmad@gmail.com>
2021-12-18 20:38:07 +0200

    Practicing with useEffect()

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const savedToLocal = localStorage.setItem("Logged", "1");
+    if (localStorage.getItem("Logged")) setIsLoggedIn(true);
+  }, []);
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
@@ -15,6 +19,7 @@ function App() {
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem("Logged");
   };
 
   return (
